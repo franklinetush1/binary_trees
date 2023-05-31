084dd5c12aabad4651d93ae2588bff9fb1ad0b94
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-05-31 11:23:03 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,8 +8,8 @@
  *         else - a pointer to the new node.
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
-    binary_tree_t* newnode;
-    newnode = (binary_tree_t*)malloc(sizeof(binary_tree_t));
+    binary_tree_t *newnode;
+    newnode = malloc(sizeof(binary_tree_t));
     if(newnode == NULL){
         return NULL;
     }
