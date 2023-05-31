3a09f4b0d4507d1580f26151cb03df2a5a589632
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-05-31 11:24:25 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,5 +17,5 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value){
     newnode->n = value;
     newnode->left = NULL;
     newnode->right = NULL;
-    return parent;
+    return (parent);
 }
