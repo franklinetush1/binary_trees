c85c78816c2a2985ae3d88173e8eb77f7d851b28
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-01 21:39:53 +0300

    Update 2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -20,6 +20,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent->right != NULL)
 	{
 		binary_tree_t *old;
+		old = parent->right;
 		new_rightnode->right = old;
 		parent->right->parent = new_rightnode;
 	}
