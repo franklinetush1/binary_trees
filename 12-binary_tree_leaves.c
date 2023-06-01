6329585fbec9cd7368e2f454c9c6093bff9934e5
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-01 18:01:05 +0300

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,12 +13,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree){
     {
         return(0);
     }
-    if (tree->left==NULL && tree->right==NULL){
-        return(1);
-    }
-    else{
-        return(0);
-    }    
+    count+= (!tree->left && !tree->right) ? 1 : 0;   
     count+= binary_tree_leaves(tree->left);
 	count+= binary_tree_leaves(tree->right);
     return(count);
