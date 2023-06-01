d3d63766db0f3e44bb12e2bdc9649676a0f5ec25
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-01 23:24:56 +0300

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -23,3 +23,30 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	else
 		return (0);
 }
+
+/**
+ * binary_tree_is_full - checks if  binary tree is full
+ * @tree: a pointer to the root node
+ * Return: 1 if true, 0 if false
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	int x;
+
+	if (tree == NULL)
+		return (0);
+
+	if(tree->left != NULL && tree->right != NULL)
+	{
+		x = binary_tree_is_full(tree->left);
+		if (x == 0)
+			return (x);
+		x = binary_tree_is_full(tree->right);
+	}	
+	else if (tree->left == NULL && tree->right == NULL)
+		x = 1;
+	else
+		x = 0;
+
+	return (x);
+}
