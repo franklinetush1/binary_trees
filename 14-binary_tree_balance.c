51faaba7af62bf92ee95b302f8053337614deaf4
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-01 23:16:56 +0300

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -23,13 +23,15 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
+	size_t leftt = 0, rightt = 0;
+	if (tree == NULL)
 	{
-		size_t l = 0, r = 0;
-
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-		return ((l > r) ? l : r);
+		return 0;
 	}
-	return (0);
+	else
+	{	
+		leftt = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		rightt = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((leftt > rightt) ? leftt : rightt);
+	}
 }
