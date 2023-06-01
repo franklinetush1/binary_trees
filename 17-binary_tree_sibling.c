8d0110221fc3a95b01b4291151c3032cd6ed6d44
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-01 21:05:43 +0300

    Update 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,12 +6,13 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL)
+	if (node == NULL)
 		return (NULL);
-  
+	if (node->parent == NULL)
+		return (NULL);  
 	if (node->parent->left == node){
 		return (node->parent->right);
-  }
+  	}
 	else{
     return (node->parent->left);
   }
