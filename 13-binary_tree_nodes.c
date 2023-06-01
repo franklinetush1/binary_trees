787439b2b3d9cdbcdf0a8bc6d43a04e02b339e1b
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-01 22:20:55 +0300

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,12 +8,11 @@
  * else return node count.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree) {
+    size_t count = 0;
     if (tree == NULL) {
         return 0;
-    }
-    
-    size_t count = 0;
-    
+    }    
+      
     if (tree->left != NULL || tree->right != NULL) {
         count++;
     }
