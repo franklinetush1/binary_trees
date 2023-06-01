744648b469b1ebd841fffd1c852cfb94c6a6aa59
franklinetush1 <119009474+franklinetush1@users.noreply.github.com>
2023-06-01 20:57:51 +0300

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -49,3 +49,19 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	return (x);
 }
+/**
+ * binary_tree_size - find size of a binary tree
+ * @tree: a pointer to the root node of the tree to find size
+ * Return: the size, 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size_left, size_right;
+
+	if (tree == NULL)
+		return (0);
+
+	size_left = binary_tree_size(tree->left);
+	size_right= binary_tree_size(tree->right);
+	return (1 + size_left + size_right);
+}
